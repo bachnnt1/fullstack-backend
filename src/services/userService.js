a8eaea007a6ac0385bb8bd7179dc8860b0771b29
bachnnt <bachcvb@gmail.com>
2022-02-20 09:09:52 +0700

    Additionally add firstname

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,7 +8,7 @@ let handleUserLogin = (email, password) => {
       let isExit = await checkUserEmail(email);
       if (isExit) {
         let user = await db.User.findOne({
-          attributes: ["email", "roleId", "password"],
+          attributes: ["email", "roleId", "password", "firstName"],
           where: { email: email },
           raw: true,
         });
