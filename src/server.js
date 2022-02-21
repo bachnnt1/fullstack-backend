86f622600c9dc508acecb11ecbbbd071e2bb6860
bachnnt <bachcvb@gmail.com>
2022-02-21 16:55:53 +0700

    Update migration: using BLOB

--- src/server.js
+++ src/server.js
@@ -9,12 +9,15 @@ require("dotenv").config(); // giúp chạy được process.env
 let app = express();
 
 var corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true };
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb" }));
 
 viewEngine(app);
 initWebRoutes(app);
