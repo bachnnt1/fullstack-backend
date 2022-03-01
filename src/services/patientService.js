336f568307dc353d72f342f8e8ae1ddd9088c5c1
bachnnt <bachcvb@gmail.com>
2022-03-01 15:32:07 +0700

    Send mail automatically

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,7 +1,13 @@
 import db from "../models/index";
+import nodemailer from "nodemailer";
+require("dotenv").config();
 let postAppointment = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
+      await sendSimpleEmail({
+        receiveEmail: data.email,
+        patientName: data.fullName,
+      });
       let user = await db.User.findOrCreate({
         where: { email: data.email },
         defaults: {
@@ -34,6 +40,27 @@ let postAppointment = (data) => {
   });
 };
 
+let sendSimpleEmail = async (data) => {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_APP, // generated ethereal user
+      pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Bách bạo chúa" <bachcvb@gmail.com>', // sender address
+    to: data.receiveEmail, // list of receivers
+    subject: "Thông tin đặt lịch khám bệnh (test full stack)", // Subject line
+    html: `<b>Hello ${data.patientName}</b>`, // html body
+  });
+};
+
 module.exports = {
   postAppointment,
+  sendSimpleEmail,
 };
